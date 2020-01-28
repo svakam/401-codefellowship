f942b0a9e29f7d98f2c447276e7a540ade2d4900
Vik Akam <vikramakam@hotmail.com>
2020-01-28 12:24:19 -0800

    initial commit from Spring init along with database config

---
+++ src/test/java/com/vik/CodeFellowShip/CodeFellowShipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vik.CodeFellowShip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeFellowShipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
