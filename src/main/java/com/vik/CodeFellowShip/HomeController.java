f8268c631aedac3f9a78cf03235ae91d266515f1
Vik Akam <vikramakam@hotmail.com>
2020-01-30 13:43:49 -0800

    model m passing in, header if/else

--- src/main/java/com/vik/CodeFellowShip/HomeController.java
+++ src/main/java/com/vik/CodeFellowShip/HomeController.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
@@ -25,10 +26,14 @@ public class HomeController {
     private PasswordEncoder encoder;
 
     @GetMapping("/")
-    public String getHome(Principal p) {
+    // model m to pass in variables
+    public String getHome(Principal p, Model m) {
         // take in Principal p (how we get access to person currently logged in), check if not null, then print name of principal
         if (p != null) {
             System.out.println(p.getName());
+            // p's name passed in, then using database repo to get username to pass it in
+            ApplicationUser user = applicationUserRepository.findByUsername(p.getName());
+            m.addAttribute("user", user);
         } else {
             System.out.println("nobody logged in");
         }
